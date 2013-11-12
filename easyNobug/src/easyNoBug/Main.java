6f96ee68915c48401a8ba43e3a6f6c0dc7dbbbfc
Jules Beroux <jules.beroux@gmail.com>
2013-11-12 15:52:47 +0100

    On remet à zéro.

---
+++ easyNobug/src/easyNoBug/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package easyNoBug;
+
+/**
+ * @author EasyNoBug
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+	}
+
+}
